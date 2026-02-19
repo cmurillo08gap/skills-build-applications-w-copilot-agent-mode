49e812c4282b956904b7cd0f0352c55597360d86
César Murillo Lizano <cmurillo@growthaccelerationpartners.com>
2026-02-19 23:52:42 +0000

    Update settings and URLs to support Codespaces environment

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-gi!qit-bk*hnz4%1ng*sj7_i*wca(1-&4rh$=6j8t(=hg!e0qo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+# Allow Codespace URL and localhost
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
